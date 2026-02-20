b6c46dae98d086b5dd1ddc6d3dff51066344013f
Arley Santana <arley.santana@hellodev.com.br>
2026-02-20 13:47:31 -0300

    Adicionando icone

--- features/main/src/commonMain/kotlin/br/com/hellodev/main/presenter/features/video_player/screen/VideoPlayerScreen.kt
+++ features/main/src/commonMain/kotlin/br/com/hellodev/main/presenter/features/video_player/screen/VideoPlayerScreen.kt
@@ -1,27 +1,15 @@
 package br.com.hellodev.main.presenter.features.video_player.screen
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawing
-import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import br.com.hellodev.design.presenter.components.icon.illustration.getDrawableIllustration
-import br.com.hellodev.design.presenter.theme.illustration.IllustrationType.IC_ARROW_LEFT
+import br.com.hellodev.design.presenter.theme.DefaultColor
 import br.com.hellodev.main.data.VideoItem
 import chaintech.videoplayer.host.MediaPlayerHost
 import chaintech.videoplayer.model.ScreenResize
+import chaintech.videoplayer.model.VideoPlayerConfig
 import chaintech.videoplayer.ui.video.VideoPlayerComposable
 
 @Composable
@@ -35,34 +23,25 @@ fun VideoPlayerScreen(
             isLooping = false,
             autoPlay = true,
             initialVideoFitMode = ScreenResize.FILL,
-            isFullScreen = true,
+            isFullScreen = true
         )
     }
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Black),
-    ) {
-        VideoPlayerComposable(
-            modifier = Modifier.fillMaxSize(),
-            playerHost = playerHost,
+    val playerConfig = remember(onBack) {
+        VideoPlayerConfig(
+            isZoomEnabled = false,
+            seekBarThumbRadius = 12.dp,
+            seekBarActiveTrackColor = DefaultColor,
+            seekBarThumbColor = DefaultColor,
+            seekBarTrackHeight = 6.dp,
+            enableBackButton = true,
+            backActionCallback = onBack,
         )
-
-        IconButton(
-            onClick = onBack,
-            modifier = Modifier
-                .align(Alignment.TopStart)
-                .windowInsetsPadding(WindowInsets.safeDrawing)
-                .padding(12.dp)
-                .clip(CircleShape)
-                .background(Color.Black.copy(alpha = 0.55f)),
-        ) {
-            Icon(
-                painter = getDrawableIllustration(IC_ARROW_LEFT),
-                contentDescription = null,
-                tint = Color.White,
-            )
-        }
     }
+
+    VideoPlayerComposable(
+        modifier = Modifier.fillMaxSize(),
+        playerHost = playerHost,
+        playerConfig = playerConfig,
+    )
 }
